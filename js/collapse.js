99567ee2f6dd1a763e9263b2aef0107b88912050
Tay <ngudai999@gmail.com>
2021-07-01 05:11:59 +0700

    Second Optimize: fix many screens

---
+++ js/collapse.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+  let w = window.innerWidth;
+  let phoneIcon = document.getElementById("phoneIcon");
+  let openIcon = document.getElementById("openIcon");
+  let closeIcon = document.getElementById("closeIcon");
+
+  if (w < 992) {
+    closeIcon.style.display = "none";
+    let collapseButton = document.getElementById("collapseButton");
+    collapseButton.addEventListener("click", switchState);
+  }
+};
+const switchState = () => {
+  if (collapseButton.ariaExpanded === "true") {
+    phoneIcon.style.display = "inline";
+    openIcon.style.display = "inline";
+    closeIcon.style.display = "none";
+  } else {
+    phoneIcon.style.display = "none";
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
+  }
+};
